998c344402fe481482ec4d3a91c0b52db7078908
Connor <connor.schilder@gmail.com>
2022-03-23 13:39:05 +0100

    Adding zips to drive did not work from colab, trying different format for zipping

--- Ev-SegNet-master/train_eager.py
+++ Ev-SegNet-master/train_eager.py
@@ -86,8 +86,8 @@ def train(loader, model, epochs=5, batch_size=2, show_loss=False, augmenter=None
             model.save_weights(name_best_model + "model" + str(epoch), save_format='tf')
             print("Written savedmodel in tf to " + name_best_model + "model" + str(epoch))
         if platform.system() != "Windows":
-            subprocess.run(["zip", "-r", "/content/DL_EvSegNet/Ev-SegNet-master/logs", "/content/drive/MyDrive/Universiteit/Deep_Learning/logs"])
-            subprocess.run(["zip", "-r", "/content/DL_EvSegNet/Ev-SegNet-master/weights/model", "/content/drive/MyDrive/Universiteit/Deep_Learning/model"])
+            subprocess.run(["zip", "-r", "/content/drive/MyDrive/Universiteit/Deep_Learning/logs", '-i', "/content/DL_EvSegNet/Ev-SegNet-master/logs",])
+            subprocess.run(["zip", "-r", "/content/drive/MyDrive/Universiteit/Deep_Learning/model", '-i', "/content/DL_EvSegNet/Ev-SegNet-master/weights/model"])
 
 
         loader.suffle_segmentation()  # shuffle training set
